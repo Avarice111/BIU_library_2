85a4f16f89472e819e2adb1cb714b8f1f4c51cb4
Kolejarzyk <pawel.dee@live.com>
2017-10-04 17:26:44 +0200

    IS14 List of Books model

--- src/app/list-of-books/list-of-books.component.ts
+++ src/app/list-of-books/list-of-books.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListOfBooksComponent implements OnInit {
 
+
+  listOfBooks= [
+    {imgUrl: 'http://ecsmedia.pl/c/pan-tadeusz-b-iext44008692.jpg', title: 'Pan Tadeusz'},
+    {imgUrl: 'http://ecsmedia.pl/c/ogniem-i-mieczem-b-iext43064008.jpg', title: 'Ogniem i Mieczem'},
+    {imgUrl: 'http://www.unreal-fantasy.pl/gfx/users/1950072231.jpg', title: 'Mistrz i Małŋorzata'},
+    {imgUrl: 'http://smart.aanomaly.net/wp-content/uploads/2014/12/lalka-boleslaw-prus.jpg', title: 'Lalka'},
+  ];
   constructor() { }
 
   ngOnInit() {
