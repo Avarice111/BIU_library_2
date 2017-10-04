cef46f1f1396b0ac46e9287cd832dacff2c85d4e
krisswoj <krisswoj@gmail.com>
2017-10-04 18:19:04 +0200

    add header #11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainAppComponent } from './main-app/main-app.component';
 import { ListOfBooksComponent } from './list-of-books/list-of-books.component';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
