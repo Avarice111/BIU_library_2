7b30fb7a80d5ce47be49ba87fa79624f0d9cbd9a
Lalunaa <meeggi2@wp.pl>
2017-10-25 16:08:25 +0200

    IS70 /advsearch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
 import { MainAppComponent } from './main-app/main-app.component';
-
+import { BookFinderComponent } from './book-finder/book-finder.component';
 
 export var routes:Routes=[
     {
@@ -8,11 +8,15 @@ export var routes:Routes=[
         component:MainAppComponent
     },
     {
+        path:'advsearch',
+        component:BookFinderComponent
+    },
+    {
         path:'',
         component:MainAppComponent
-    },
+    },   
     {
         path:'**',
         redirectTo:'/home'
-    }
+    }   
 ];
