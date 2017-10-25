7ecc1cb65c2acc2520c02061f2165b527a2e165d
s13909 <s13909@pjwstk.edu.pl>
2017-10-25 16:47:40 +0200

    Added product page button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import {Routes} from '@angular/router';
 import { MainAppComponent } from './main-app/main-app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ListOfBooksComponent } from './list-of-books/list-of-books.component';
 
 
 export var routes:Routes=[
@@ -12,7 +14,16 @@ export var routes:Routes=[
         component:MainAppComponent
     },
     {
+        path:'book',
+        component:ProductPageComponent
+    },
+    {
+        path:'books',
+        component:ListOfBooksComponent
+    },
+    {
         path:'**',
         redirectTo:'/home'
-    }
+    },
+    
 ];
