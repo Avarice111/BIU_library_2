bf8fb1b55d178b1f23ecfb7aeaae93eda090f089
Tmejs <mateusz.rzad@gmail.com>
2017-11-15 16:15:53 +0100

    IS#127 list ksizk powinna sie zmieniac, przy zmanie kategorii- kompoent powinien pobierac z adresu url nazwe lub id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,59 @@
 import {Routes} from '@angular/router';
-import { MainAppComponent } from './main-app/main-app.component';
+import {MainAppComponent} from './main-app/main-app.component';
 import {BookPageComponent} from './book-page/book-page.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
 import {RegisterComponent} from './user-profile/register/register.component';
 import {ChangePasswordComponent} from './user-profile/change-password/change-password.component';
 import {LoginComponent} from './user-profile/login/login.component';
-import { ListOfBooksComponent } from './list-of-books/list-of-books.component';
-import { BookFinderComponent } from './book-finder/book-finder.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { MessageDetailsComponent } from './message-details/message-details.component';
-import { ListOfMessagesComponent } from './list-of-messages/list-of-messages.component';
+import {ListOfBooksComponent} from './list-of-books/list-of-books.component';
+import {BookFinderComponent} from './book-finder/book-finder.component';
+import {ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {MessageDetailsComponent} from './message-details/message-details.component';
+import {ListOfMessagesComponent} from './list-of-messages/list-of-messages.component';
 import {AddBookFormComponent} from './add-book-form/add-book-form.component';
 
 
 export let routes: Routes = [
-    {
+  {
+    path: '',
+    component: MainAppComponent,
+    children: [
+      {
         path: '',
-        component: MainAppComponent,
-        children: [
-            {
-                path: '',
-                component: ListOfBooksComponent
-            },
-            {
-                path: 'book/:bookid',
-                component: BookPageComponent
-            },
-            {
-              path:'message/:messageid',
-              component:MessageDetailsComponent
-           },
+        component: ListOfBooksComponent
+      },
+      {
+        path: 'book/:bookid',
+        component: BookPageComponent
+      },
+      {
+        path: 'message/:messageid',
+        component: MessageDetailsComponent
+      },
 
-           {
-            path:'messages',
-            component:ListOfMessagesComponent
-        },
-            {
-                path: 'books',
-                component: ListOfBooksComponent
-            },
-            {
-                path: 'shoppingCart',
-                component:ShoppingCartComponent
-            },
-          {
-                path: 'advsearch',
-                component: BookFinderComponent
+      {
+        path: 'messages',
+        component: ListOfMessagesComponent
+      },
+      {
+        path: 'books/:category',
+        component: ListOfBooksComponent
+      },
+      {
+        path: 'shoppingCart',
+        component: ShoppingCartComponent
+      },
+      {
+        path: 'advsearch',
+        component: BookFinderComponent
 
-            }
-        ]
-    },
-    {
-        path: 'home',
-        redirectTo: ''
-    },
+      }
+    ]
+  },
+  {
+    path: 'home',
+    redirectTo: ''
+  },
   {
     path: 'profile',
     component: UserProfileComponent
@@ -74,9 +74,9 @@ export let routes: Routes = [
     path: 'add-book',
     component: AddBookFormComponent
   },
-    {
-        path: '**',
-        redirectTo: '/home'
-    },
+  {
+    path: '**',
+    redirectTo: '/home'
+  },
 
 ];
