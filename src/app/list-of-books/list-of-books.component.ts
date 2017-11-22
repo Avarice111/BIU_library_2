6c46197a78d5462447da2a0a358367d64f836c49
Tmejs <mateusz.rzad@gmail.com>
2017-11-22 18:16:23 +0100

    IS# gdy ładowana jest lista ksiazek ma byc widoczny trober, az do momentu załadowania listy

--- src/app/list-of-books/list-of-books.component.ts
+++ src/app/list-of-books/list-of-books.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BookItem } from '../../view-models/BookItem';
 import {ActivatedRoute, Params} from '@angular/router';
-import { BooksService } from './books.service';
+import {Observable} from 'rxjs/Observable';
+import {InMemBooksService} from '../books.inmemory.service';
+import {BookService} from '../../in-memory/service/BookService';
+import {BooksService} from './books.service';
+
 
 @Component({
   selector: 'list-of-books',
@@ -10,40 +14,41 @@ import { BooksService } from './books.service';
 })
 export class ListOfBooksComponent implements OnInit {
 
-  listOfBooks: BookItem[];
-   public static LIST_OF_BOOKS = [
-    new BookItem('http://ecsmedia.pl/c/pan-tadeusz-b-iext44008692.jpg', 'Pan Tadeusz', 1),
-    new BookItem('http://ecsmedia.pl/c/ogniem-i-mieczem-b-iext43064008.jpg', 'Ogniem i Mieczem', 2),
-    new BookItem('http://www.unreal-fantasy.pl/gfx/users/1950072231.jpg', 'Mistrz i Małŋorzata', 3),
-    new BookItem('http://smart.aanomaly.net/wp-content/uploads/2014/12/lalka-boleslaw-prus.jpg', 'Lalka', 4),
-  ];
 
+
+  listOfBooks$= new Observable<BookItem[]>();
   category: string;
+  isLoading= true;
+  model: BookItem[]= [];
 
-  constructor(private activatedRoute: ActivatedRoute, private booksService: BooksService) { }
+  constructor(private activatedRoute: ActivatedRoute, private booksService: BooksService) {
+
+  }
 
   ngOnInit() {
-
     // subscribe to router event
+    this.isLoading = true;
+
     this.activatedRoute.params.subscribe((params: Params) => {
       this.category = params['category'];
-      this.booksService.getBooks().subscribe(item=>console.log(item));
+      this.booksService.getBooks().subscribe(item => console.log(item));
     });
-
-
-
     if (this.category != null)
     {
       if (this.category.length)
         // Podmieniamy liste książek na podstawie kategorii
-        this.listOfBooks = this.getListOfBooksByCategory(this.category);
+        this.listOfBooks$ = BookService.getListOfBooksByCategory(this.category);
+    }else{
+        this.listOfBooks$ =   BookService.getListOfBooksByCategory(null);
     }
-  }
 
-  public getListOfBooksByCategory(category: any) {
-    // TODO trzeba zrobić filtrowanie
-    return ListOfBooksComponent.LIST_OF_BOOKS;
-  }
 
+    this.listOfBooks$.subscribe(list => {
+      this.model = list;
+      this.isLoading = false;
+    });
+
+
+  }
 
 }
